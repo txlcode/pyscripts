a92f0661d5be8d405a04e7511c930b309b76b8bc
txlcode <38198967+txlcode@users.noreply.github.com>
2018-10-12 14:23:07 +0800

    upload zabbix_dingding.py

---
+++ zabbix_dingding.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+import sys
+import os
+
+headers = {'Content-Type': 'application/json;charset=utf-8'}
+api_url = "https://oapi.dingtalk.com/robot/send?access_token=d79e3c32b590756ef9f9c06a5ec138771d65802d17a4c6af45fda0fc91d97e3b"
+
+def msg(text):
+    json_text= {
+     "msgtype": "text",
+        "text": {
+            "content": text
+        },
+        "at": {
+            "atMobiles": [
+                "15196638082"
+            ],
+            "isAtAll": True
+        }
+    }
+    print requests.post(api_url,json.dumps(json_text),headers=headers).content
+
+if __name__ == '__main__':
+    text = sys.argv[1]
+    msg(text)
